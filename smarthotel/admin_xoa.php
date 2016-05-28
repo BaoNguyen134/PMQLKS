c30b9afee01357e3a6fcfb2e53a0195d80e14567
BaoNguyen <nguyentuanbao93@gmail.com>
2016-05-28 14:13:02 +0700

    Lệnh xóa đã xóa bỏ toàn bộ thư mục và user tạo ra lúc đăng ký

--- smarthotel/admin_xoa.php
+++ smarthotel/admin_xoa.php
@@ -1,7 +1,26 @@
 <?php
 	require('../database.php');
 	$conn = connectDatabase();
+	remove_directory("../ZKS_".$_GET['id']);
 	$sql="delete from dangkyphanmem where email='".$_GET['id']."'";
 	mysql_query($sql);
 	header("location:admin.php");
+?>
+
+<?php
+function remove_directory($dir){
+  if($handle = opendir("$dir")){
+     while(false !== ($item = readdir($handle))){
+       if($item != "." && $item != ".."){
+          if(is_dir("$dir/$item")){
+             remove_directory("$dir/$item");
+          }else{
+          unlink("$dir/$item"); // đến đây xóa hết các file
+         }
+        }
+     } 
+     closedir($handle);
+     rmdir($dir); // đến đây xóa foder
+   }
+}
 ?>
--- tao_khachsan.php
+++ tao_khachsan.php
@@ -3,22 +3,11 @@
 		$ten = "ZKS_".$tenkhachsan;
 		mkdir($ten);
 		$index = fopen($ten."/index.php", "w") or die("Unable to open file!");
-		$txt = "<?php
-	require('../database.php');
-	if(!isset('$'_SESSION['currEmail'])){
-		header('location:../login.php');
-	} 
-	else {
-		if(!isset('$'_SESSION['currAdmin'])){
-			header('location:../index.php');
-		}
-		else{}
-	}
-?>\n";
+		$txt = "Xin Chao Bao Nguyen";
 		fwrite($index, $txt);
 		fclose($index);
 		$hotro = fopen($ten."/hotro.php", "w") or die("Unable to open file!");
-		$txt = "Bảo Nguyễn\n";
+		$txt = "Xin Chao Bao Nguyen";
 		fwrite($hotro, $txt);
 		fclose($hotro);
 	}
