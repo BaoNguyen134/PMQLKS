a0b832e4be14ecbf31d73a68f8a97cb965ccb943
BaoNguyen <nguyentuanbao93@gmail.com>
2016-05-28 08:56:44 +0700

    sửa lỗi phần xóa trong admin

--- smarthotel/admin_xoa.php
+++ smarthotel/admin_xoa.php
@@ -1,16 +1,7 @@
 <?php
-require('../database.php');
-	if(!isset($_SESSION['currEmail'])){
-		header("location:../login.php");
-	} 
-	else {
-		if(!isset($_SESSION['currAdmin'])){
-			header("location:../index.php");
-		}
-		else{}
-	}
- $conn = connectDatabase();
- $sql="delete from dangkyphanmem where email='".$_GET['id']."'";
-              mysql_query($sql);
-			  include'admin.php';
+	require('../database.php');
+	$conn = connectDatabase();
+	$sql="delete from dangkyphanmem where email='".$_GET['id']."'";
+	mysql_query($sql);
+	header("location:admin.php");
 ?>
