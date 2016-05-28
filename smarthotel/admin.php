9d6cb8c0127b718043ba0ed3509a30930cd5a776
BaoNguyen <nguyentuanbao93@gmail.com>
2016-05-28 09:41:45 +0700

    thay đổi lại form sửa kích hoạt

--- smarthotel/admin.php
+++ smarthotel/admin.php
@@ -13,7 +13,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head><link rel="stylesheet" type="text/css" href="admin.css">
+<head>
+<link rel="stylesheet" type="text/css" href="admin.css">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <title>QUẢN LÝ KHÁCH SẠN</title>
@@ -22,8 +23,8 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script> 
-<script src="../js/init.js"></script> 
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="../js/init.js"></script>
 <script src="../js/materialize.js"></script>
 </head>
 <body>
@@ -33,22 +34,22 @@
     <ul class="right hide-on-med-and-down">
       <li><a href="#"><b class="z-depth-3">QUẢN LÝ KHÁCH HÀNG</b></a></li>
       <li>
-	  <?php 
+        <?php 
 		if(!isset($_SESSION['currEmail'])){
 		?>
         <a href="login.php"><b class="z-depth-3">ĐĂNG NHẬP</b></a>
-      <?php 
+        <?php 
 		} else {
 			echo "<a><b class='z-depth-3'>Xin chào! ".$_SESSION['currEmail']."</b></a>";
 			}
-	  	?>  
-        </li>
+	  	?>
+      </li>
       <li>
-      <?php 
+        <?php 
 		if(!isset($_SESSION['currEmail'])){
 		?>
-      <a href="#"><b class="z-depth-3">ĐĂNG KÝ</b></a>
-      <?php 
+        <a href="#"><b class="z-depth-3">ĐĂNG KÝ</b></a>
+        <?php 
 		} else {
 			echo "<a href='../logout.php'><b class='z-depth-3'>THOÁT</b></a>";
 			}
@@ -57,22 +58,22 @@
     </ul>
     <ul id="nav-mobile" class="side-nav orange">
       <li><a href="#"><b class="z-depth-3  white-text">QUẢN LÝ KHÁCH HÀNG</b></a></li>
-	  <?php 
+      <?php 
 		if(!isset($_SESSION['currEmail'])){
 		?>
-        <a href="login.php"><b class="z-depth-3 white-text">ĐĂNG NHẬP</b></a>
+      <a href="login.php"><b class="z-depth-3 white-text">ĐĂNG NHẬP</b></a>
       <?php 
 		} else {
 			echo "<a><b class='z-depth-3 white-text'>".$_SESSION['currEmail']."</b></a>";
 			}
-	  	?>  
-        </li>
+	  	?>
+      </li>
       <li>
-      <?php 
+        <?php 
 		if(!isset($_SESSION['currEmail'])){
 		?>
-      <a href="#"><b class="z-depth-3">ĐĂNG KÝ</b></a>
-      <?php 
+        <a href="#"><b class="z-depth-3">ĐĂNG KÝ</b></a>
+        <?php 
 		} else {
 			echo "<a href='../logout.php'><b class='z-depth-3 white-text'>THOÁT</b></a>";
 			}
@@ -81,11 +82,11 @@
     </ul>
     <a href="#" data-activates="nav-mobile" class="button-collapse white-text"><i class="material-icons">menu</i></a> </div>
 </nav>
-<!-- end menu --> 
+<!-- end menu -->
 <div align="center" style="margin-bottom: 50px">
   <h5 class="blue-grey-text"><b class="z-depth-3">DANH SÁCH KHÁCH HÀNG</b></h5>
-    <table class="highlight z-depth-3" style="width:100%;">
-      <tr >
+  <table class="highlight z-depth-3" style="width:100%;">
+    <tr >
       <th><span class="blue-grey-text">ROLE</span></th>
       <th><span class="blue-grey-text">TT KÍCH HOẠT</span></th>
       <th><span class="blue-grey-text">HỌ TÊN</span></th>
@@ -95,7 +96,7 @@
       <th><span class="blue-grey-text">ĐỊA CHỈ</span></th>
       <th><span class="blue-grey-text">SỐ LƯỢNG PHÒNG</span></th>
     </tr>
-	<?php 
+    <?php 
 		$conn = connectDatabase();
 		if(isset($_POST['update'])){
 $role=$_POST['role'];
@@ -139,9 +140,9 @@ $email=$_POST['email'];
 			}
 		}
 		mysql_close($conn);
-	?> 
-    </table>
-  </div>
+	?>
+  </table>
+</div>
 </div>
 <script>
  var hv=<?php echo json_encode($hv);?>;
@@ -149,12 +150,7 @@ $email=$_POST['email'];
 	 var p=hv.filter(function(item) {
     return (item.email === e.id);
 });    console.log(p);
-        $('#ten').val(p['0'].ho_ten);
-		$('#sdt').val(p['0'].dien_thoai);
 		$('#email').val(p['0'].email);
-		$('#tenks').val(p['0'].ten_khach_san);
-		$('#diachi').val(p['0'].dia_chi);
-		$('#slphong').val(p['0'].so_luong_phong);
 		$('#tt').val(p['0'].kichhoat);
 		$('#role').val(p['0'].role);
 
@@ -186,42 +182,19 @@ $(document).ready(function() {
 	
 });
  </script>
-      <div id="update" class="login">
-    <p class="login_title">Thay Đổi Thông Tin Sản Phẩm</p>
-     <a href="#" class="close"><img src="close.png" class="img-close" title="Close Window" alt="Close" /></a>
-            <form method="post" class="login-content" action="#">
-            <label class="TenSanPham">
-            <span>Tên Thành Viên</span>
-            <input id="ten" name="ten" value="" type="text" autocomplete="on"  readonly="readonly">
-            </label>
-            <label >
-            <span>Số Điện Thoại</span>
-            <input id="sdt" name="sdt" value="" type="text" placeholder="Số Điện Thoại">
-            </label>
-            <label >
-            <span>Email</span>
-            <input id="email" name="email" value="" type="text" placeholder="Email" readonly>
-            </label>
-            <label >
-            <span>Tên Khách Sạn</span>
-            <input id="tenks"  name="tenks" value="" type="text" placeholder="Tên Khách Sạn">
-            </label>
-             <label >
-            <span>Địa Chỉ</span>
-            <input id="diachi"  name="diachi" value="" type="text" placeholder="Địa Chỉ">
-            </label>
-             <label >
-            <span>Số Lượng Phòng</span>
-            <input id="slphong"  name="slphong" value="" type="number" placeholder="Số Lượng Phòng">
-            </label>
-             <label >
-            <span>TTrang Thái Kích Hoạt</span>
-            <input id="tt"  name="tt" value="" type="text" placeholder="Trạng Thái Kích Hoạt">
-            </label>
-             <label >
-            <span>ROLE</span>
-            <input id="role"  name="role" value="" type="number" placeholder="Role">
-            </label>
-            <button class="button submit-button"   type="submit" name="update">UPDATE</button>       
-            </form>
-        </div>     
+<div id="update" class="login">
+  <p class="login_title" style="font-size:22px">THAY ĐỔI TRẠNG THÁI KÍCH HOẠT</p>
+  <a href="#" class="close"><img src="close.png" class="img-close" title="Close Window" alt="Close" /></a>
+  <form method="post" class="login-content" action="#">
+    <label><span class="blue-grey-text"><b style="font-size:18px">EMAIL</b></span>
+      <input class="blue-grey-text" id="email" name="email" value="" type="email" readonly>
+    </label>
+    <label><span class="blue-grey-text"><b style="font-size:18px">KÍCH HOẠT</b></span>
+      <input id="tt"  name="tt" value="" type="text">
+    </label>
+    <label><span class="blue-grey-text"><b style="font-size:18px">ROLE</b></span>
+      <input id="role"  name="role" value="" type="text">
+    </label>
+    <button  align="center" class="btn submit-button" type="submit" name="update"><b>CẬP NHẬT</b></button>
+  </form>
+</div>
